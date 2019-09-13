cee2a7d75de6e21ef8ffc65b4e08b4365b7e1265
joas99 <54931975+joas99@users.noreply.github.com>
2019-09-13 17:19:31 +0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+
+package org.iuea.oop;
+
+import org.iuea.oop.views.LoginView;
+
+
+public class MainClass {
+
+   
+    public static void main(String[] args) {
+        
+        LoginView login=new LoginView();
+        
+    }
+     
+}
---
+++ modall/CourseUnits.java
@@ -0,0 +1,22 @@
+
+package org.iuea.oop.modall;
+
+/*Group members 
+ MADRAGULE Andrito Joas  18/208/BIT-J
+ DUT Daniel Kur          19/550/BSCS-S
+ FAROUQ MUHAMMAD Ahmed   18/225/BIT-J
+ MOKILI LILALA Levi      18/0053/BSSE-J
+ AGANZE DUNIA Ghislain   18/138/BSSE-j
+*/
+//creating the courseUnits class that holds details of a courseUnit
+public class CourseUnits {
+    //creating the attributes of the class
+    static String cUnitname;
+    static String creditUnit;
+    static String cUnitdescription;
+    static String cUnitcontent;
+    static String coursecode;
+    static String cUnityear;
+    
+      
+}
