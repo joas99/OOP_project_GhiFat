940e4f0174429baf05e22a39d1ff8b7c9ec38ff2
joas99 <54931975+joas99@users.noreply.github.com>
2019-10-04 14:38:12 +0300

    Add files via upload

---
+++ src/modal/Student.java
@@ -0,0 +1,31 @@
+
+package org.iuea.oop.modall;
+/*Group members 
+ MADRAGULE Andrito Joas  18/208/BIT-J
+ DUT Daniel Kur          19/550/BSCS-S
+ FAROUQ MUHAMMAD Ahmed   18/225/BIT-J
+ MOKILI LILALA Levi      18/0053/BSSE-J
+ AGANZE DUNIA Ghislain   18/138/BSSE-j
+*/
+
+//creating the student class to hold details of a student
+public class Student{
+    //creating variables to hold details of a student
+    static String fname;
+    static String lname;
+    static String email;
+    static String course;
+    
+    //creating a constructor that help us initialize the attributes of a student
+    public Student(String a,String b,String c,String d){
+               //information about the student
+               fname=a;
+               lname=b;
+               email=c;
+               course=d;
+               
+    }
+    
+    
+}
+
